b61b01c36986696e844244be214d1bddabc8b5c2
resulyildirim <57195609+resulyildirim@users.noreply.github.com>
2020-05-25 23:09:52 +0100

    update task recently

--- src/test/java/test/task.java
+++ src/test/java/test/task.java
@@ -3,5 +3,7 @@ package test;
 public class task {
     public static void main(String[] args) {
         System.out.println( "Hello new task");
+        System.out.println("Hello world")
+            //This is from github
     }
 }
