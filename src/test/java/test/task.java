f20e85eca9eca7a39149512da793c4eb22227e01
Resul YILDIRIM <resul7723@gmail.com>
2020-05-26 01:18:57 +0100

    added method

--- src/test/java/test/task.java
+++ src/test/java/test/task.java
@@ -3,7 +3,11 @@ package test;
 public class task {
     public static void main(String[] args) {
         System.out.println( "Hello new task");
-        System.out.println("Hello world")
+        System.out.println("Hello world");
             //This is from github
+    }
+
+    public  void hi( ) {
+        System.out.println("Hi from InteliJ");
     }
 }
