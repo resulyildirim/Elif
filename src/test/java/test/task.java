7064fa0482b3a972793959f2c29a889492515ca2
resulyildirim <57195609+resulyildirim@users.noreply.github.com>
2020-05-26 01:07:54 +0100

    return metod

--- src/test/java/test/task.java
+++ src/test/java/test/task.java
@@ -4,8 +4,10 @@ public class task {
     public static void main(String[] args) {
         System.out.println( "Hello new task");
         System.out.println("Hello world")
-            //This is from github
-            
+            //This is from github      
            //I did new sentences in github
+    }
+    public String hello(){
+        return "Hello from GitHub";
     }
 }
