7cf757b52811bfdebee2f1f5d5f6c8364b01a1aa
resulyildirim <57195609+resulyildirim@users.noreply.github.com>
2020-05-26 12:43:57 +0100

    update poll

--- src/test/java/test/poll.java
+++ src/test/java/test/poll.java
@@ -1,9 +1,9 @@
-
 package test;
-
 public class poll {
     public static void main(String[] args) {
         System.out.println("Hello poll  How are you");
         //This new trying fetch and merge
     }
-}
+    public void m1(){
+        System.out.println("Hello GitHub");
+  }
